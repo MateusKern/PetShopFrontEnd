ff19ce297f97679233fd7fbe832cc9f815857b1e
Mateus Kern <mateus.g.kern@gmail.com>
2022-07-04 16:24:53 -0300

    add login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: AppComponent }
+]
+
+@NgModule({
+    imports: [
+      CommonModule,
+      RouterModule.forRoot(routes),
+    ],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {
+  }  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    LoginComponent
+   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
