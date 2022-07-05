63ee234af4bbdd6716a0f66f44b2a265d1317473
Mateus Kern <mateus.g.kern@gmail.com>
2022-07-05 09:48:45 -0300

    Quando não encontra a rota direciona para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'home', component: AppComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ]
 
 @NgModule({
