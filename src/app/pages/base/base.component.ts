a511b602cf54ac6d1b8b6b00df5005f210ee967e
Mateus Kern <mateus.g.kern@gmail.com>
2022-08-08 15:27:42 -0300

    colocando o font awesome

--- src/app/pages/base/base.component.ts
+++ src/app/pages/base/base.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base.component.css']
 })
 export class BaseComponent implements OnInit {
+  isCollapsed: boolean = false;
   constructor() { }
 
   ngOnInit() {
